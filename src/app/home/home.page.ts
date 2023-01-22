279ce5663f837d334911fc011ca4a3b1672ccee2
George Mburu (meetjojie) <mburugeorge692@gmail.com>
2023-01-22 19:21:07 +0300

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import tt from '@tomtom-international/web-sdk-maps';
+import { map } from 'rxjs';
+import { runInThisContext } from 'vm';
+
+//let marker = new tt.Marker().setLngLat({lng: 0,lat: 9, }).addTo(this.map);
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  @ViewChild('map', { static: true }) mapContainer: ElementRef;
+  map: any;
+  
+  constructor(private el: ElementRef) { 
+    this.mapContainer = el;
+ }
 
-  constructor() {}
+  ngOnInit() {
+   let map= tt.map({
+      key: '9b5jg1sFr3zCwbomsIwM9srR6pItgIHi',
+      container: this.mapContainer.nativeElement,
+      zoom: 2,
 
+    });
+    
+    map.addControl(new tt.FullscreenControl());
+    map.addControl(new tt.NavigationControl());
+  }
 }
---
+++ src/app/login/event.js
@@ -0,0 +1,4 @@
+document.getElementById("loginbtn").addEventListener ("click", function() {
+    window.location.href="https://www.javatpoint.com/javascript-onclick-event";
+ 
+});
